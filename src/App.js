591c1cf493a528046f07539fadb4551bf75eed30
Amit Kumar <15000amitkumar@gmail.com>
2023-11-28 23:21:00 +0530

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import data from "./data";
 function App() {
   console.log(data);
   return (
-    <div className="flex flex-col w-[100vw] justify-center items-center bg-gray-200">
-      <h1 className="text-green-600">Our Testimonials</h1>
-      <div></div>
-      <Testimonials reviews={data}></Testimonials>
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">Our Testimonials</h1>
+        <div className=" bg-violet-400 h-[4px] w-[1/5] mt-1 "></div>
+        <Testimonials reviews={data}></Testimonials>
+      </div>
     </div>
   );
 }
